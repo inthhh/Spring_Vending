2d46c334d01e812c5dbb7192a3d40f812f82c705
inthhh <glass005533@gmail.com>
2025-04-09 21:43:41 +0900

    refactor: api path version 추가

--- src/main/java/com/study/springboot/ApiController.java
+++ src/main/java/com/study/springboot/ApiController.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @CrossOrigin(origins = "*")
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/vi")
 public class ApiController {
     
     private List<ProductDto> productList = new ArrayList<>();
